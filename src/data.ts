40453929fde03e7047b92eb4fbba543ed1ac6575
Jordan Skomal <jskomal@gmail.com>
2022-08-10 13:41:20 -0600

    refactor images to use imports

--- src/data.ts
+++ src/data.ts
@@ -1,92 +1,120 @@
+import bluebrocade from './assets/bluebrocade.jpg'
+import dance from './assets/dance.jpg'
+import embrace from './assets/embrace.png'
+import fire from './assets/fire.png'
+import frock from './assets/frock.png'
+import glorious from './assets/glorious.jpg'
+import golden from './assets/golden.jpg'
+import maskedMeeting from './assets/maskedmeeting.jpg'
+import masquerade from './assets/masquerade.jpg'
+import moonlitMeadow from './assets/moonlitmeadow.jpg'
+import office from './assets/office.png'
+import picnic from './assets/picnic.jpg'
+import pirate from './assets/pirate.jpg'
+import prarie from './assets/prairie.jpg'
+import redRennaisance from './assets/redrenaissance.jpg'
+import regency from './assets/regency.jpg'
+import ribbons from './assets/ribbons.jpg'
+import roses from './assets/roses.jpg'
+import ruffles from './assets/ruffles.jpg'
+import scroll from './assets/scroll.jpg'
+import shine from './assets/shine.png'
+import smolder from './assets/smolder.png'
+import snow from './assets/snow.jpg'
+import sparkles from './assets/sparkles.jpg'
+import stripes from './assets/stripes.png'
+import wildWest from './assets/wildwest.jpg'
+import windswept from './assets/windswept.jpg'
+
 export const covers = [
-  './assets/bluebrocade.jpg',
-  './assets/dance.jpg', 
-  './assets/embrace.png',
-  './assets/fire.png',
-  './assets/frock.png',
-  './assets/glorious.jpg',
-  './assets/golden.jpg',
-  './assets/maskedmeeting.jpg',
-  './assets/masquerade.jpg',
-  './assets/moonlitmeadow.jpg',
-  './assets/office.png',
-  './assets/picnic.jpg',
-  './assets/pirate.jpg',
-  './assets/prairie.jpg',
-  './assets/redrenaissance.jpg',
-  './assets/regency.jpg',
-  './assets/ribbons.jpg',
-  './assets/roses.jpg',
-  './assets/ruffles.jpg',
-  './assets/scroll.jpg',
-  './assets/shine.png',
-  './assets/smolder.png',
-  './assets/snow.jpg',
-  './assets/sparkles.jpg',
-  './assets/stripes.png',
-  './assets/wildwest.jpg',
-  './assets/windswept.jpg',
-];
+  bluebrocade,
+  dance,
+  embrace,
+  fire,
+  frock,
+  glorious,
+  golden,
+  maskedMeeting,
+  masquerade,
+  moonlitMeadow,
+  office,
+  picnic,
+  pirate,
+  prarie,
+  redRennaisance,
+  regency,
+  ribbons,
+  roses,
+  ruffles,
+  scroll,
+  shine,
+  smolder,
+  snow,
+  sparkles,
+  stripes,
+  wildWest,
+  windswept
+]
 
 export const titles = [
-  "Passionate Moonlight",
+  'Passionate Moonlight',
   "Love's Misery",
-  "Roses and Flame",
-  "Innocent Roses",
-  "Silk and Sense",
-  "Hearts Aflame",
-  "Fiery Passion",
-  "Stolen Hearts",
-  "Secrets and Silk",
-  "Dreams of Fire",
-  "Lovers and Enemies",
+  'Roses and Flame',
+  'Innocent Roses',
+  'Silk and Sense',
+  'Hearts Aflame',
+  'Fiery Passion',
+  'Stolen Hearts',
+  'Secrets and Silk',
+  'Dreams of Fire',
+  'Lovers and Enemies',
   "Passion's Embrace",
-  "Harbinger by Moonlight",
-  "Rouge Red",
-  "Moonlit Mysteries",
-  "Crimson Roses",
+  'Harbinger by Moonlight',
+  'Rouge Red',
+  'Moonlit Mysteries',
+  'Crimson Roses',
   "Destiny's Fires",
-  "Proposals and Passion",
-  "Silk Wedding",
-  "Masked Seduction",
-  "Crimson Masquerade",
-  "Stolen Rubies",
-  "Emerald Eyes",
-  "Sapphire Skies",
-  "Opal Passions"
-];
+  'Proposals and Passion',
+  'Silk Wedding',
+  'Masked Seduction',
+  'Crimson Masquerade',
+  'Stolen Rubies',
+  'Emerald Eyes',
+  'Sapphire Skies',
+  'Opal Passions'
+]
 
 export const descriptors = [
-  "passion",
-  "glory",
-  "romance",
-  "woe",
-  "sorrow",
-  "pain",
-  "ardor",
-  "devotion",
-  "excitement",
-  "fervor",
-  "rage",
-  "spirit",
-  "zeal",
-  "ecstasy",
-  "fire",
-  "storms",
-  "tempests",
-  "rapture",
-  "ire",
-  "jealousy",
-  "exhilaration",
-  "bliss",
-  "enchantment",
-  "paradise",
-  "calamity",
-  "disaster",
-  "heartache",
-  "misfortune",
-  "agony",
-  "curses",
-  "blessings",
-  "melancholy"
-];
+  'passion',
+  'glory',
+  'romance',
+  'woe',
+  'sorrow',
+  'pain',
+  'ardor',
+  'devotion',
+  'excitement',
+  'fervor',
+  'rage',
+  'spirit',
+  'zeal',
+  'ecstasy',
+  'fire',
+  'storms',
+  'tempests',
+  'rapture',
+  'ire',
+  'jealousy',
+  'exhilaration',
+  'bliss',
+  'enchantment',
+  'paradise',
+  'calamity',
+  'disaster',
+  'heartache',
+  'misfortune',
+  'agony',
+  'curses',
+  'blessings',
+  'melancholy'
+]
