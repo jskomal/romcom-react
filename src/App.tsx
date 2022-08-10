64584a91c4c36f650ff8f8aa0d1deb590c978725
Jordan Skomal <jskomal@gmail.com>
2022-08-10 13:42:09 -0600

    Load random cover on page load

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,27 @@
-import './App.css'
+import { useState, useEffect } from 'react'
 import { titles, covers, descriptors } from './data'
+import { getRandomIndex, Cover } from './utils'
 import Header from './components/Header'
+import Main from './components/Main'
+
+export type TcurrentCover = null | Cover
 
 const App = () => {
+  const [currentCover, setCurrentCover] = useState<TcurrentCover>(null)
+
+  useEffect(() => {
+    const coverImgSrc = covers[getRandomIndex(covers)]
+    const title = titles[getRandomIndex(titles)]
+    const descriptor1 = descriptors[getRandomIndex(descriptors)]
+    const descriptor2 = descriptors[getRandomIndex(descriptors)]
+    const cover = new Cover(coverImgSrc, title, descriptor1, descriptor2)
+    setCurrentCover(cover)
+  }, [])
+
   return (
-    <div className='App'>
+    <div>
       <Header />
+      <Main currentCover={currentCover} />
     </div>
   )
 }
