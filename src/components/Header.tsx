6f95588125fdc3a386ab0888e1086077443701e7
Jordan Skomal <jskomal@gmail.com>
2022-08-11 01:49:01 -0600

    Add conditional rendering to Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,38 @@
 import React from 'react'
+import { TcurrentCover } from '../App'
 import './Header.css'
 
-type Props = {}
+type HeaderProps = {
+  createNewCover: () => void
+  saveCover: (savedCovers: TcurrentCover[], currentCover: TcurrentCover) => void
+  toggleSavedOpen: () => void
+  toggleMakeOpen: () => void
+  isMainHidden: boolean
+  returnHome: () => void
+  savedCovers: TcurrentCover[]
+  currentCover: TcurrentCover
+}
 
-const Header = (props: Props) => {
+const Header = ({
+  createNewCover,
+  saveCover,
+  toggleSavedOpen,
+  toggleMakeOpen,
+  isMainHidden,
+  returnHome,
+  savedCovers,
+  currentCover
+}: HeaderProps) => {
+  const clickSave = () => {
+    saveCover(savedCovers, currentCover)
+  }
   return (
     <header>
-      <button>Show New Random Cover</button>
-      <button>Save Cover</button>
-      <button>Show Saved Covers</button>
-      <button>Make Your Own Cover</button>
+      {isMainHidden && <button onClick={returnHome}>Home</button>}
+      {!isMainHidden && <button onClick={createNewCover}>Show New Random Cover</button>}
+      {!isMainHidden && <button onClick={clickSave}>Save Cover</button>}
+      {!isMainHidden && <button onClick={toggleSavedOpen}>Show Saved Covers</button>}
+      {!isMainHidden && <button onClick={toggleMakeOpen}>Make Your Own Cover</button>}
     </header>
   )
 }
