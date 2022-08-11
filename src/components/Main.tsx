97d5b2deb2120308e9a193856e0014e15092b519
Jordan Skomal <jskomal@gmail.com>
2022-08-11 01:49:31 -0600

    Refactor div > article

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -8,13 +8,13 @@ type MainProps = {
 const Main = ({ currentCover }: MainProps) => {
   return (
     <main>
-      <div className='cover'>
+      <article className='cover'>
         <img src={currentCover?.cover} alt={currentCover?.title} />
         <h1>{currentCover?.title}</h1>
         <p>
           A tale of {currentCover?.tagline1} and {currentCover?.tagline2}
         </p>
-      </div>
+      </article>
     </main>
   )
 }
